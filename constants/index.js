815140e42371528c566081be62d098351d82824b
Emmanuel Ifeanyi MECHIE <64092981+emmanuelByte@users.noreply.github.com>
2023-05-13 20:26:34 +0100

    init

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+import { CONTRACT } from "./abi";
+export const TOKEN_CONTRACT_ABI = CONTRACT.TOKEN.abi;
+export const TOKEN_CONTRACT_ADDRESS = CONTRACT.TOKEN.address;
+export const EXCHANGE_CONTRACT_ABI = CONTRACT.EXCHANGE.abi;
+export const EXCHANGE_CONTRACT_ADDRESS = CONTRACT.EXCHANGE.address;
